400a3be79d8c01de71c182e45aa65e26ee443fda
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-03-03 19:26:41 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,98 +34,43 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  collection = []
-  movie_key = movies_collection[0]
-  movie = movie_with_director_name(name,movie_key)
-  movie_index = 0
-  while movie_index < movie.length do
-    collection << {:director_name=>name}
-    movie_index+=1
-  end
-  collection
-
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+movie_hash = []
+i = 0
+while i < movies_collection.length do
+  movie_data = movie_with_director_name(name,movies_collection[i])
+  movie_hash << movie_data
+  i+=1
+end
+movie_hash
 end
 
 
 def gross_per_studio(collection)
-
-  index = 0
-  movie_table = []
-  while index < collection.length
-    table = collection[index]
-    data = movie_with_director_name('director',table)
-    movie_table << data
-    index+=1
-    end
-    movie_table
-
-  result = {}
+  studio_gross = {}
   i=0
-  while i<movie_table.length do
-    studio = movie_table[i][:studio]
-    if !result[studio]
-      result[studio] = movie_table[i][:worldwide_gross]
+  while i < collection.length do
+    if !studio_gross[collection[i][:studio]]
+      studio_gross[collection[i][:studio]]=collection[i][:worldwide_gross]
     else
-      result[studio] += movie_table[i][:worldwide_gross]
-    end
+      studio_gross[collection[i][:studio]]+=collection[i][:worldwide_gross]
+      end
     i+=1
-  end
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
-    result
+    end
+    studio_gross
 end
 
 def movies_with_directors_set(x)
-  directors_database
-  y = directors_database
-  dir_array = []
-  director_index = 0
-  while director_index < x.length do
-  director = x[director_index][:name]
-  movie_index = 0
-  while movie_index < x[director_index][:movies].length do
-  movie = x[director_index][:movies][movie_index][:title]
-  gross = y[director_index][:movies][movie_index][:worldwide_gross]
-  studio = y[director_index][:movies][movie_index][:studio]
-    dir_array << [{:title => movie, :director_name => director, :worldwide_gross => gross, :studio => studio}]
-    movie_index+=1
+  i = 0
+  movies_list = []
+  while i < x.length do
+    j = 0
+    while j < x[i][:movies].length
+      movies_list << [movie_with_director_name(x[i][:name],x[i][:movies][j])]
+      j+=1
+    end
+    i+=1
   end
-  director_index+=1
-end
- dir_array
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  # source
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+movies_list
 end
 
 # ----------------    End of Your Code Region --------------------
@@ -137,5 +82,3 @@ def studios_totals(nds)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
   return gross_per_studio(movies_with_director_names)
 end
-
-pp directors_database
